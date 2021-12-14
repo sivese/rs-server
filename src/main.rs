f5908492668dd3ef4da1ee08beb73c612e9e7db2
lee <driversivese@gmail.com>
2021-12-14 18:25:57 +0900

    multi thread

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 */
 
 mod svr;
+mod thread;
 
 fn main() {
 
--- src/svr/server.rs
+++ src/svr/server.rs
@@ -3,6 +3,10 @@ struct Server{
 
 }
 
+use crate::thread::pool::ThreadPool;
+
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -14,30 +18,41 @@ fn handle_connection(mut stream : TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let content = fs::read_to_string("./svr/hel.html").unwrap();
-        let res = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            content.len(),
-            content
-        );
-
-        stream.write(res.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status, file) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    }
+    else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
     }
     else {
-        // Do other something
-    }
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string("404.html").unwrap();
+
+    let res = format!(
+        "{}\r\nCotent-Length: {}\r\n\r\n{}",
+        status,
+        contents.len(),
+        contents
+    );
+
+    stream.write(res.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    
 }
 
 #[test]
 fn server_test() {
     let lisner = TcpListener::bind("127.0.0.1:1818").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in lisner.incoming() {
         let s = stream.unwrap();
-
+        
         handle_connection(s);
     }
 }
